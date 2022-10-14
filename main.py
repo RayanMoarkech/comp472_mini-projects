9da96849fdd0e0e412e1e8b77004626acdb33ba5
Rayan Moarkech <rayanzone@hotmail.com>
2022-10-14 14:02:53 -0400

    Organized main and read file

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # This is a sample Python script.
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Loads the data from a file
+# Takes in a file_name string
+# Returns a json dictionary
+def load_data(file_name):
+    file = open(file_name,)
+    data_json = json.load(file)
 
+    print(f"The dataset {file_name} is loaded")
+    return data_json
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Main method of the code
+def main():
+    # Get the dataset
+    data_json = load_data(file_name='goemotions.json')
+
+    return
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
