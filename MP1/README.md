f12232d95db61bf9141d7ad5d499614f810da9fd
Lujain Khalaf <>
2022-11-16 23:01:22 -0500

    Moved to separate folder

---
+++ MP1/README.md
@@ -0,0 +1,87 @@
+# comp472_mini-projects
+[This repo](https://github.com/RayanMoarkech/comp472_mini-projects) currently holds the mini-project 1 of COMP472.
+
+![Python 3.6](https://img.shields.io/badge/python-v3.7-blue)
+
+<p align="left"> 
+    <a href="https://www.python.org" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> 
+    </a> 
+    <a href="https://pytorch.org/" target="_blank" rel="noreferrer"> 
+        <img src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width="40" height="40"/> 
+    </a> 
+    <a href="https://scikit-learn.org/" target="_blank" rel="noreferrer"> 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg" alt="scikit_learn" width="40" height="40"/> 
+    </a>
+</p>
+
+---
+
+## The Team
+
+Team Name: `Codeine`
+
+<table>
+    <tr>
+        <td align="center">
+            <a href="https://github.com/RayanMoarkech">
+                <img src="https://avatars.githubusercontent.com/u/34872331?v=4" width="120px;" alt="Profile Picture"/>
+                <br>
+                <b>Rayan Moarkech</b>
+            </a>
+            <br>
+            40089399
+        </td>
+        <td align="center">
+            <a href="https://github.com/LujainKhalaf">
+                <img src="https://avatars.githubusercontent.com/u/67845184?v=4" width="120px;" alt="Profile Picture"/>
+                <br>
+                <b>Lujain Khalaf</b>
+            </a>
+            <br>
+            40086330
+        </td>
+        <td align="center">
+            <a href="https://github.com/samimerhi">
+                <img src="https://avatars.githubusercontent.com/u/50461308?v=4" width="120px;" alt="Profile Picture"/>
+                <br>
+                <b>Sami Merhi</b>
+            </a>
+            <br>
+            40136648
+        </td>
+    </tr>
+</table>
+
+---
+
+## Install Pre-requisites
+Some packages need to be installed to complete the project. Do the following:
+```
+pip3 install -r requirements.txt
+```
+
+## Dataset
+The dataset is stored in gzip `./goemotions.json`.
+Description:
+```
+It is a modified version of the original GoEmotion dataset, created by [Demszky et al., 2020]
+The dataset we will use for this assignment is a modified version of the original GoEmotion, 
+where only posts annotated with a single emotion (and a single sentiment) are kept, 
+and the data is formatted in json. 
+The json file contains triplets made of the post, its emotion and its sentiment.
+```
+The dataset is organized in an array with each element containing:
+```
+[
+ human-annotated Reddit comment,
+ emotions,
+ sentiments: positive/negative/ambiguous/neutral
+] 
+```
+
+## Running the Code
+
+Run the main function inside the file `main.py`.
+This will run all the assignment code in chronological manner. 
+Function calls are maid in the main function to call specific tasks for each exercise number.
---
+++ MP1/compute_performance.py
@@ -0,0 +1,86 @@
+# Library imports
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+# Global variables
+file_name = 'performance.txt'
+
+
+data_class_org = {
+  0: "reddit comments",
+  1: "emotions",
+  2: "sentiments"
+}
+
+
+# Used to get the data
+# Avoid duplications
+# Returns:
+# target_name: emotions or sentiments based on the index
+# cv_train_fit: the CountVectorizer.fit_transform of the comments in the train data set
+# target_true_train: array containing the target true value at index (emotions=1,sentiments=2) in the train data set
+# cv_test_transform: the CountVectorizer.transform of the comments in the test data set
+# target_true_test: array containing the target true value at index (emotions=1,sentiments=2) in the test data set
+def get_true_cv_target_data(data_train, data_test, index):
+    # Create a numpy array with the comments
+    comments_train_array = [data_array[0] for data_array in data_train]
+    comments_train_np = np.array(comments_train_array)
+
+    # Get the training data comments
+    cv = CountVectorizer()
+    cv_train_fit = cv.fit_transform(comments_train_np)
+
+    # Get the target train true values
+    target_true_train = [data_array[index] for data_array in data_train]
+
+    # Get the test set
+    comments_test_array = [data_array[0] for data_array in data_test]
+    comments_test_np = np.array(comments_test_array)
+    cv_test_transform = cv.transform(comments_test_np)
+
+    # Get the target test true values
+    target_true_test = [data_array[index] for data_array in data_test]
+
+    return data_class_org[index], cv_train_fit, target_true_train, cv_test_transform, target_true_test
+
+
+# Creates a new file called `performance.txt`
+# If the file exists, it flushes all the content inside it
+def flush_performance_file():
+    with open(file_name, 'w') as f:
+        f.write('')
+
+
+# 2.4: Save info in performance file
+# Appends to the file
+def write_to_performance_file(model_description, target_true_test, target_predict):
+    # Get the confusion matrix
+    matrix = compute_confusion_matrix(target_true_test=target_true_test, target_predict=target_predict)
+
+    # Get the classification report
+    report = compute_classification_report(target_true_test=target_true_test, target_predict=target_predict)
+
+    # Print details to file
+    with open(file_name, 'a') as f:
+        f.write(model_description + '\n')
+        f.write('' + '\n')
+        f.write('The Confusion Matrix:' + '\n')
+        np.savetxt(fname=f, X=matrix, newline='\n')
+        f.write('' + '\n')
+        f.write('The Classification Report:' + '\n')
+        f.write(report + '\n')
+        f.write('' + '\n')
+        f.write('----------------------------------------------------------------------------------------------' + '\n')
+        f.write('' + '\n')
+
+
+# Computes the confusion matrix
+def compute_confusion_matrix(target_true_test, target_predict):
+    return confusion_matrix(y_true=target_true_test, y_pred=target_predict)
+
+
+# Computes the classification report
+def compute_classification_report(target_true_test, target_predict):
+    return classification_report(y_true=target_true_test, y_pred=target_predict)
