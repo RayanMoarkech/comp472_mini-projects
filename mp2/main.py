900912a6b27806a10e882730467ea12f3e825933
Rayan Moarkech <rayanzone@hotmail.com>
2022-11-21 16:52:10 -0500

    Cleanup

--- mp2/main.py
+++ mp2/main.py
@@ -1,4 +1,4 @@
-from file import get_boards
+from file import get_games
 
 
 # Global Variables
@@ -8,7 +8,9 @@ input_file = "metadata/input/sample-input.txt"
 # Main method of the code
 def main():
     print("Hello")
-    get_boards(filename=input_file)
+
+    # Get a list of RushHour objects from an input file
+    games = get_games(filename=input_file)
 
 
 # Press the green button in the gutter to run the script.
