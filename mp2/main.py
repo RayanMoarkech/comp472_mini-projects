aeb60e32e5c1b95671a40abd0cab248610e9ef93
Rayan Moarkech <rayanzone@hotmail.com>
2022-11-21 10:23:31 -0500

    Prepare main

---
+++ mp2/main.py
@@ -0,0 +1,17 @@
+# Library imports
+import json
+import nltk
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+
+# Main method of the code
+def main():
+    print("Hello")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
