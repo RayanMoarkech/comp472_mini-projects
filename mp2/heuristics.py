3f72e113f1def49e46cd19ddd05ac6367777abf3
Sami <smerhi.sm@gmail.com>
2022-11-28 18:18:21 -0500

    h1

--- mp2/heuristics.py
+++ mp2/heuristics.py
@@ -1,5 +1,18 @@
 from rush_hour import RushHour
 
+# Gets the heuristic h1 of a rush hour game
+# Takes in RushHour object
+# Returns the heuristic value
+def get_h1(rush_hour: RushHour):
+    ambulance = rush_hour.get_vehicle("A")
+    front = ambulance.get_front()
+    heuristic = 0
+    blocking_vehicle = ""
+    for x in range(front.x + 1, 6):
+        if rush_hour.board[front.y][x] != "." and rush_hour.board[front.y][x] != blocking_vehicle:
+            blocking_vehicle = rush_hour.board[front.y][x]
+            heuristic += 1
+    return heuristic
 
 # Gets the heuristic h2 of a rush hour game
 # Takes in RushHour object
