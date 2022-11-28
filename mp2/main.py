94c0294f5d461cf784643d144f0380778bcac203
Rayan Moarkech <rayanzone@hotmail.com>
2022-11-28 16:44:25 -0500

    Modifications to RushHour class

--- mp2/main.py
+++ mp2/main.py
@@ -1,4 +1,5 @@
 from file import get_games
+from rush_hour import Move
 
 
 # Global Variables
@@ -11,6 +12,40 @@ def main():
 
     # Get a list of RushHour objects from an input file
     games = get_games(filename=input_file)
+
+    # Board manipulation
+    sample_mod(game=games[0])
+
+
+# A sample function with some board manipulations with RushHour methods
+def sample_mod(game):
+    for line in game.board:
+        for el in line:
+            print(el, end=" ")
+        print()
+
+    print()
+
+    # Move M down 2 spots
+    game.move_vehicle(Move.DOWN, 2, "M")
+    for line in game.board:
+        for el in line:
+            print(el, end=" ")
+        print()
+
+    print()
+
+    # Move A right once
+    game.move_vehicle(Move.RIGHT, 1, "A")
+    for line in game.board:
+        for el in line:
+            print(el, end=" ")
+        print()
+
+    print()
+    
+    # Get vehicle position
+    print(game.get_vehicle("A").positions)
 
 
 # Press the green button in the gutter to run the script.
