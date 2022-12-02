b15b7361b09810a9b9c1b4df1488e27bf7a7aee3
Rayan Moarkech <rayanzone@hotmail.com>
2022-12-02 12:13:55 -0500

    cleanup main

--- mp2/main.py
+++ mp2/main.py
@@ -1,11 +1,7 @@
 from file import get_games
-from rush_hour import Move
 from heuristics import get_h1, get_h2, get_h4, get_h5, get_h6
-from gbfs import greedy_bfs
 from a_star_solver import main as a_star_solver
 from gbfs import greedy_bfs as gbfs
-
-import copy
 
 
 # Global Variables
@@ -27,79 +23,29 @@ def main():
     # print(get_h5(rush_hour=games[4]), get_h6(rush_hour=games[4]))
     # print(get_h5(rush_hour=games[5]), get_h6(rush_hour=games[5]))
 
-    # A Star Algorithm with h1
-    #a_star_solver(rush_hours=games, heuristic_used=1)
+    # GBFS Algorithm with h1
     # gbfs(rush_hours=games, heuristic_used=1)
-    #sample_mod(initial_game=games[0])
 
-    # A Star Algorithm with h2
-    # a_star_solver(rush_hours=games, heuristic_used=2)
+    # A Star Algorithm with h1
+    a_star_solver(rush_hours=games, heuristic_used=1)
+
+    # GBFS Algorithm with h2
     # gbfs(rush_hours=games, heuristic_used=2)
 
+    # A Star Algorithm with h2
+    a_star_solver(rush_hours=games, heuristic_used=2)
+
+    # GBFS Algorithm with h3
+    # gbfs(rush_hours=games, heuristic_used=3)
+
     # A Star Algorithm with h3
-    # a_star_solver(rush_hours=games, heuristic_used=3)
-    #gbfs(rush_hours=games, heuristic_used=3)
+    a_star_solver(rush_hours=games, heuristic_used=3)
+
+    # GBFS Algorithm with h4
+    # gbfs(rush_hours=games, heuristic_used=4)
 
     # A Star Algorithm with h4
     # a_star_solver(rush_hours=games, heuristic_used=4)
-    gbfs(rush_hours=games, heuristic_used=4)
-
-
-# A sample function with some board manipulations with RushHour methods
-def sample_mod(initial_game):
-    # Deep copy the array to be modified and used
-    game = copy.deepcopy(initial_game)
-
-    # for line in game.board:
-    #     for el in line:
-    #         print(el, end=" ")
-    #     print()
-
-    # print()
-
-    # # Move M down 2 spots
-    # game.move_vehicle(Move.DOWN, 2, "M")
-    # for line in game.board:
-    #     for el in line:
-    #         print(el, end=" ")
-    #     print()
-
-    # print()
-
-    # Move A right once
-    # game.move_vehicle(Move.RIGHT, 1, "A")
-    # for line in game.board:
-    #     for el in line:
-    #         print(el, end=" ")
-    #     print()
-
-    #valid_states = game.get_all_next_valid_states({})
-
-    # for state in valid_states:
-    #     for line in state.board:
-    #         for el in line:
-    #             print(el, end=" ")
-    #         print()
-    #     print()
-
-    # for state in valid_states:
-    #     print("h1", get_h1(state['game']))
-    #     print("h2", get_h2(state))
-
-    #greedy_bfs({'rushHour': game, 'vehicleName': ''}, get_h1)
-    #     print("h1", get_h1(state))
-    #     print("h2", get_h2(state))
-    # for state in valid_states:
-    #     for line in state['rushHour'].board:
-    #         for el in line:
-    #             print(el, end=" ")
-    #         print()
-    #     print()
-
-    #greedy_bfs(game, get_h1)
-
-    # Get vehicle position
-    # print(game.get_vehicle("A").positions)
 
 
 # Press the green button in the gutter to run the script.
