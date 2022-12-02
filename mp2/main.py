8049edbcda11198167142f69bc60059c2c8c2058
Rayan Moarkech <rayanzone@hotmail.com>
2022-12-02 12:20:01 -0500

    Update main.py

--- mp2/main.py
+++ mp2/main.py
@@ -13,9 +13,6 @@ def main():
     # Get a list of RushHour objects from an input file
     games = get_games(filename=input_file)
 
-    # Board manipulation
-    #sample_mod(initial_game=games[1])
-
     # print(get_h5(rush_hour=games[0]), get_h6(rush_hour=games[0]))
     # print(get_h5(rush_hour=games[1]), get_h6(rush_hour=games[1]))
     # print(get_h5(rush_hour=games[2]), get_h6(rush_hour=games[2]))
@@ -27,19 +24,19 @@ def main():
     # gbfs(rush_hours=games, heuristic_used=1)
 
     # A Star Algorithm with h1
-    a_star_solver(rush_hours=games, heuristic_used=1)
+    # a_star_solver(rush_hours=games, heuristic_used=1)
 
     # GBFS Algorithm with h2
     # gbfs(rush_hours=games, heuristic_used=2)
 
     # A Star Algorithm with h2
-    a_star_solver(rush_hours=games, heuristic_used=2)
+    # a_star_solver(rush_hours=games, heuristic_used=2)
 
     # GBFS Algorithm with h3
     # gbfs(rush_hours=games, heuristic_used=3)
 
     # A Star Algorithm with h3
-    a_star_solver(rush_hours=games, heuristic_used=3)
+    # a_star_solver(rush_hours=games, heuristic_used=3)
 
     # GBFS Algorithm with h4
     # gbfs(rush_hours=games, heuristic_used=4)
