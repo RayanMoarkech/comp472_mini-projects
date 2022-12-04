6e825260763e966115a36db3511752b47fa1ef7a
Rayan Moarkech <rayanzone@hotmail.com>
2022-12-04 11:16:16 -0500

    Enable main

--- mp2/main.py
+++ mp2/main.py
@@ -6,7 +6,7 @@ from file import create_analysis_file
 from uniform_cost_search import main as uniform_cost_search
 
 # Global Variables
-input_file = "metadata/input/sample-input.txt"
+input_file = "metadata/input/input.txt"
 
 # Main method of the code
 def main():
@@ -18,43 +18,43 @@ def main():
     #     write_to_input_file(get_random_line())
 
     # Get a list of RushHour objects from an input file
-    # games = get_games(filename=input_file)
+    games = get_games(filename=input_file)
 
     # Get a list of RushHour objects from random
     # games = generate_random_games(count=5)
 
     # UCS
-    # uniform_cost_search(rush_hours=games)
+    uniform_cost_search(rush_hours=games)
 
     # GBFS Algorithm with h1
-    # gbfs(rush_hours=games, heuristic_used=1)
+    gbfs(rush_hours=games, heuristic_used=1)
 
     # A Star Algorithm with h1
-    # a_star_solver(rush_hours=games, heuristic_used=1)
+    a_star_solver(rush_hours=games, heuristic_used=1)
 
     # GBFS Algorithm with h2
-    # gbfs(rush_hours=games, heuristic_used=2)
+    gbfs(rush_hours=games, heuristic_used=2)
 
     # A Star Algorithm with h2
-    # a_star_solver(rush_hours=games, heuristic_used=2)
+    a_star_solver(rush_hours=games, heuristic_used=2)
 
     # GBFS Algorithm with h3
-    # gbfs(rush_hours=games, heuristic_used=3)
+    gbfs(rush_hours=games, heuristic_used=3)
 
     # A Star Algorithm with h3
-    # a_star_solver(rush_hours=games, heuristic_used=3)
+    a_star_solver(rush_hours=games, heuristic_used=3)
 
     # GBFS Algorithm with h4
-    # gbfs(rush_hours=games, heuristic_used=4)
+    gbfs(rush_hours=games, heuristic_used=4)
 
     # A Star Algorithm with h4
-    # a_star_solver(rush_hours=games, heuristic_used=4)
+    a_star_solver(rush_hours=games, heuristic_used=4)
 
     # GBFS Algorithm with h5
-    # gbfs(rush_hours=games, heuristic_used=5)
+    gbfs(rush_hours=games, heuristic_used=5)
 
     # A Star Algorithm with h5
-    # a_star_solver(rush_hours=games, heuristic_used=5)
+    a_star_solver(rush_hours=games, heuristic_used=5)
 
 
 # Press the green button in the gutter to run the script.
